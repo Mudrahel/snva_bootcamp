67c30152bd9f2229931634e5a877b3bd55b271f1
Ryhor Mudrahel <ryhor.murahel@gmail.com>
2023-12-20 15:14:12 -0500

    Initial commit

---
+++ src/main/java/ryhor/mudrahel/snva/bootcamp/quizplatform/Quiz.java
@@ -0,0 +1,4 @@
+package ryhor.mudrahel.snva.bootcamp.quizplatform;
+
+public interface Quiz {
+}
---
+++ src/main/java/ryhor/mudrahel/snva/bootcamp/quizplatform/QuizPlatform.java
@@ -0,0 +1,8 @@
+package ryhor.mudrahel.snva.bootcamp.quizplatform;
+
+public class QuizPlatform {
+
+    public static void run() {
+        System.out.println("Work in progress");
+    }
+}
